7aab4f71d6cd8d82c720e646245ae521fb150a78
bopjiang <bopjiang@163.com>
2018-08-26 23:16:38 +0800

    update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 set -x
 
-curl -s -X POST http://localhost:8080/users -H "Content-Type:application/json" -d '{"name":"papa","age": 26}' | jq
-curl -s -X POST http://localhost:8080/users -H "Content-Type:application/json" -d '{"name":"mama","age": 25}' | jq
-curl -s -X POST http://localhost:8080/users -H "Content-Type:application/json" -d '{"name":"son","age": 1}'   | jq
-curl -s -X GET http://localhost:8080/users | jq
-curl -X DELETE http://localhost:8080/users/1 
-curl -s -X GET http://localhost:8080/users | jq
-curl -X POST http://localhost:8080/batch -H 'Content-Type:multipart/mixed; boundary=END_OF_PART' --data-binary @batch_delete.txt
-curl -s -X GET http://localhost:8080/users | jq
+curl -i -s -X POST http://localhost:8080/users -H "Content-Type:application/json" -d '{"name":"papa","age": 26}'
+curl -i -s -X POST http://localhost:8080/users -H "Content-Type:application/json" -d '{"name":"mama","age": 25}'
+curl -i -s -X POST http://localhost:8080/users -H "Content-Type:application/json" -d '{"name":"son","age": 1}'
+curl -i -s -X GET http://localhost:8080/users
+curl -i -s -X DELETE http://localhost:8080/users/1
+curl -i -s -X GET http://localhost:8080/users
+curl -i -s -X POST http://localhost:8080/batch -H 'Content-Type:multipart/mixed; boundary=END_OF_PART' --data-binary @batch_delete.txt
+curl -i -s -X GET http://localhost:8080/users
 
